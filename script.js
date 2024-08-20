544e9031701a252c3fa8b8811c51cbe97c7fb6cb
chiky340 <halleychiky@gmail.com>
2024-08-20 23:28:16 +0200

    this takes a array of marks finds average and gives a score

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const output = fizzBuzz(31);
-console.log(output);
-
-function fizzBuzz(input){
-    if(typeof input !== 'number'){
-        return 'Not a number';
-    }
-    else if((input%3===0)  && (input%5===0)){
-        return 'fizzbuzz'
-    }
-    else if(input%3===0){
-        return 'fizz';
-    }
-    else if(input%5===0){
-        return 'buzz';
-    }
-   return input;
-}
-
+console.log(sum(10));
+function sum(limit){
+    let sum =0;
+    for(let i=0;i<=limit;i++)
+        if(i%3==0 || i%5==0)
+            sum += i;
+        return sum;     
+};
 
 
 
